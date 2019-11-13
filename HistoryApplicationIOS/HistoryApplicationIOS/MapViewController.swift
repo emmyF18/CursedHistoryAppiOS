a272ee5c5dfe0140be8506b03b747f60c50af1c5
sam.adeniyi <sam.adeniyi@my.simpson.edu>
2019-11-13 10:33:04 -0600

    Adding screen names and View Controllers

---
+++ HistoryApplicationIOS/HistoryApplicationIOS/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  HistoryApplicationIOS
+//
+//  Created by Sam Adeniyi on 11/11/19.
+//  Copyright © 2019 Sam Adeniyi. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
