b48931ea3af69bdd77b46c7a6e431714e95a101b
sam.adeniyi <sam.adeniyi@my.simpson.edu>
2019-11-15 10:45:49 -0600

    Building List first changes

--- HistoryApplicationIOS/HistoryApplicationIOS/BuildingListViewController.swift
+++ HistoryApplicationIOS/HistoryApplicationIOS/BuildingListViewController.swift
@@ -12,6 +12,8 @@ class BuildingListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
 
         // Do any additional setup after loading the view.
     }
